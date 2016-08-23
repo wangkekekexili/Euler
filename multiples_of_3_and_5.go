e85d9d20b5d3e0ad433996f7acaa27bcda41408a
Ke Wang <ke@squareup.com>
2016-08-23 10:33:23 -0400

    Change from != to <

--- multiples_of_3_and_5.go
+++ multiples_of_3_and_5.go
@@ -2,7 +2,7 @@ package euler
 
 func sumOfMultiplesOf3And5Below(target int) int {
 	sum := 0
-	for i := 1; i != target; i++ {
+	for i := 1; i < target; i++ {
 		if i%3 == 0 || i%5 == 0 {
 			sum += i
 		}
